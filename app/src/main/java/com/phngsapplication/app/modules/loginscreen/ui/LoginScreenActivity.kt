b625466b92eb895489d09aa120a518231e2a1600
HP 440 G7 <mn113839@gmail.com>
2023-04-05 04:52:55 +0700

    MinhSmart Login and SignUp

--- app/src/main/java/com/phngsapplication/app/modules/loginscreen/ui/LoginScreenActivity.kt
+++ app/src/main/java/com/phngsapplication/app/modules/loginscreen/ui/LoginScreenActivity.kt
@@ -3,7 +3,10 @@ package com.phngsapplication.app.modules.loginscreen.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.phngsapplication.app.R
 import com.phngsapplication.app.appcomponents.base.BaseActivity
 import com.phngsapplication.app.databinding.ActivityLoginScreenBinding
@@ -11,19 +14,64 @@ import com.phngsapplication.app.modules.forgetpassscreenone.ui.ForgetPassScreenO
 import com.phngsapplication.app.modules.homepage.ui.HomepageActivity
 import com.phngsapplication.app.modules.loginscreen.`data`.viewmodel.LoginScreenVM
 import com.phngsapplication.app.modules.signupscreen.ui.SignupScreenActivity
+import org.koin.android.ext.android.bind
 import kotlin.String
 import kotlin.Unit
 
-class LoginScreenActivity : BaseActivity<ActivityLoginScreenBinding>(R.layout.activity_login_screen)
+class LoginScreenActivity : AppCompatActivity()
     {
+      private lateinit var binding: ActivityLoginScreenBinding
   private val viewModel: LoginScreenVM by viewModels<LoginScreenVM>()
+  //private lateinit var binding: ActivityLoginScreenBinding
+  private lateinit var firebaseAuth: FirebaseAuth
 
-  override fun onInitialized(): Unit {
+      override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.txtSignUp.setOnClickListener{
+          val intent = Intent(this, SignupScreenActivity::class.java)
+          startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener{
+          val email = binding.etEmailOne.text.toString()
+          val pass = binding.etGroupEightyFive.text.toString()
+
+          if(email.isNotEmpty() && pass.isNotEmpty()){
+            firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+              if(it.isSuccessful){
+                val intent = Intent(this, HomepageActivity::class.java)
+                startActivity(intent)
+              }else{
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+              }
+            }
+          }else{
+            Toast.makeText(this, "Empty Fields Are not Valid !!", Toast.LENGTH_SHORT).show()
+          }
+
+
+        }
+
+      }
+//      override fun onStart() {
+//        super.onStart()
+//
+//        if(firebaseAuth.currentUser != null){
+//          val intent = Intent(this, HomepageActivity::class.java)
+//          startActivity(intent)
+//        }
+//      }
+fun onInitialized(): Unit {
     viewModel.navArguments = intent.extras?.getBundle("bundle")
     binding.loginScreenVM = viewModel
   }
 
-  override fun setUpClicks(): Unit {
+  fun setUpClicks(): Unit {
     binding.txtSignUp.setOnClickListener {
       val destIntent = SignupScreenActivity.getIntent(this, null)
       startActivity(destIntent)
--- app/src/main/java/com/phngsapplication/app/modules/signupscreen/ui/SignupScreenActivity.kt
+++ app/src/main/java/com/phngsapplication/app/modules/signupscreen/ui/SignupScreenActivity.kt
@@ -3,7 +3,12 @@ package com.phngsapplication.app.modules.signupscreen.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.phngsapplication.app.R
 import com.phngsapplication.app.appcomponents.base.BaseActivity
 import com.phngsapplication.app.databinding.ActivitySignupScreenBinding
@@ -14,15 +19,48 @@ import kotlin.String
 import kotlin.Unit
 
 class SignupScreenActivity :
-    BaseActivity<ActivitySignupScreenBinding>(R.layout.activity_signup_screen) {
+  AppCompatActivity() {
+  private lateinit var binding: ActivitySignupScreenBinding
   private val viewModel: SignupScreenVM by viewModels<SignupScreenVM>()
 
-  override fun onInitialized(): Unit {
+  private lateinit var firebaseAuth: FirebaseAuth
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = ActivitySignupScreenBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    firebaseAuth = FirebaseAuth.getInstance()
+
+    binding.btnSignUpOne.setOnClickListener {
+      val email = binding.etEmailOne.text.toString()
+      val pass = binding.etGroupNinety.text.toString()
+      val name = binding.etGroupNinetyTwo.text.toString()
+
+      if(email.isNotEmpty() && pass.isNotEmpty() && name.isNotEmpty()){
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+          if(it.isSuccessful){
+            val intent = Intent(this, LoginScreenActivity::class.java)
+            startActivity(intent)
+          }else{
+            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+          }
+        }
+      }else{
+        Toast.makeText(this, "Empty Fields Are not Valid !!", Toast.LENGTH_SHORT).show()
+      }
+    }
+
+  }
+
+
+  fun onInitialized(): Unit {
     viewModel.navArguments = intent.extras?.getBundle("bundle")
     binding.signupScreenVM = viewModel
   }
 
-  override fun setUpClicks(): Unit {
+  fun setUpClicks(): Unit {
     binding.btnSignUpOne.setOnClickListener {
       val destIntent = LoginScreenActivity.getIntent(this, null)
       startActivity(destIntent)
